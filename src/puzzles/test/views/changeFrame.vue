9f33abaa31f22ca42fe09e80a739261e873a3a55
CyberFei <im.gf@live.com>
2019-07-01 15:48:00 +0800

    Create changeFrame.vue
    新增基座切换 demo

---
+++ src/puzzles/test/views/changeFrame.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page">
+    <button @click="changeFrame">点我切换一个基座（灰度测试）</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "changeFrame",
+  methods: {
+    changeFrame() {
+      if (localStorage.getItem("frame") == "dashboard")
+        localStorage.setItem("frame", "demo");
+      else localStorage.setItem("frame", "dashboard");
+      location.reload();
+    }
+  }
+};
+</script>
